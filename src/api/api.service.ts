abbc263e0e8f28e27f0be163a37a438203e1309c
Muha11ad <abdushukurovmukhammadjon70@gmail.com>
2025-01-28 11:56:13 +0500

    fix : origin

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
+import { ApiError } from './types';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { ApiError } from './types';
 
 @Injectable()
 export class ApiService {
@@ -13,17 +13,25 @@ export class ApiService {
     return axios.create({
       baseURL: this.baseUrl,
       headers: {
+        options: {
+          origin: this.configService.get<string>('ORIGIN'),
+        },
         'Content-Type': 'application/json',
       },
       withCredentials: true,
     });
   }
+
   public async getData<T>(endpoint: string) {
     try {
       const response = await this.apiClient().get<T>(endpoint);
       return response.data;
-    } catch (error) {
-      console.error('Error fetching data from API:', error);
+    } catch (e) {
+      return {
+        message: e.response?.data?.message || 'An error occurred',
+        error: e.response?.data?.error || 'Unknown Error',
+        statusCode: e.response?.status || 500,
+      };
     }
   }
 
